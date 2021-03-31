b74252b36ddc960d4191eabf568757530a30d848
Albranco <42916864+drero@users.noreply.github.com>
2021-03-31 00:44:52 -0300

    teste
    teste

---
+++ precache-manifest.6f3c392d1a12ac77f5f9614c1ff5432d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61d59eb483adc9df7f91c8555d78a06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86847d0837fa29ee6f70",
+    "url": "/static/css/main.2bf3447f.chunk.css"
+  },
+  {
+    "revision": "ba6a5fdf07312c18c6c8",
+    "url": "/static/js/2.301cccdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.301cccdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86847d0837fa29ee6f70",
+    "url": "/static/js/main.ab031291.chunk.js"
+  },
+  {
+    "revision": "f3a586af524502814c7f",
+    "url": "/static/js/runtime-main.0f0c390f.js"
+  }
+]);
